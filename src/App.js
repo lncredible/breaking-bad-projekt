dc8c5bc0360dc28a88031377e197af4b7328b965
Ali <alilibazed@hotmail.com>
2021-05-09 01:30:39 +0200

    Added header and css styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Header from './components/ui/Header'
 const App = ()=> {
   return (
     <div className="container">
-      Hello
+      <Header/>
     </div>
   );
 }
