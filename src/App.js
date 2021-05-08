abf8bdadc9688068d714443285abf026657dd6e5
Ali <alilibazed@hotmail.com>
2021-05-09 01:37:43 +0200

    Fetch data with Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
 import Header from './components/ui/Header'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 const App = ()=> {
+  const[items, setItems] = useState([])
+  const[isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=>{
+    const fetchItems = async () =>{
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data)
+    }
+    fetchItems();
+  }, [])
   return (
     <div className="container">
       <Header/>
