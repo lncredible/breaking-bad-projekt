74577f63b2922f96db98a842fec459c799cd8d05
Ali <alilibazed@hotmail.com>
2021-05-09 02:45:57 +0200

    Added spinner animation while items are fetching

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
 
+
+
+//Om isLoading är false, då har items inte fetchats än, vi visar "Loading" till användaren. Annars gå igenom alla items och visa dem i CharacterItem komponent
 const CharacterGrid = ({items, isLoading}) => {
-    return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
+    return isLoading ? (<Spinner/>) : (<section className="cards">
         {items.map(item => (
             <CharacterItem key={item.char_id} item ={item}></CharacterItem>
         ))}
