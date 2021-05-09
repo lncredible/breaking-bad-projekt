bc32123de92d84541e2af1f958ef05759b526777
Ali <alilibazed@hotmail.com>
2021-05-09 02:42:31 +0200

    Added functionality to search + some css styling

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import Search from './components/ui/Search'
 import axios from 'axios'
 
 const App = ()=> {
-  const[items, setItems] = useState([])
-  const[isLoading, setIsLoading] = useState(true)
 
-  useEffect(()=>{
-    const fetchItems = async () =>{
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
 
-      console.log(result.data)
-      setItems(result.data)
-      setIsLoading(false)
+  const[items, setItems] = useState([]) //Här ska vi lägga in items efter vi fetchar från API
+  const[isLoading, setIsLoading] = useState(true) //isLoading state true, när vi har lyckats fetcha items blir den till false
+  const[query, setQuery] = useState('') //Search hantering, state ändras till texten från search-boxen, annars empty sträng som fetchar alla items
+
+  useEffect(()=>{ //useEffect används för att fetcha items, vi lägger till [query] som dependency för att kalla om när användaren söker efter karaktärer
+    const fetchItems = async () =>{ //Denna inner funktionen används för att kunna använda async och await
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`) //Fetchar items från API
+
+      setItems(result.data) //Efter items är fetchade lägger vi in dem i items
+      setIsLoading(false)//isLoading state till false, då vi har fått tag på items
     }
     fetchItems();
     
-  }, [])
+  }, [query]) //När query ändras körs den här metoden om
+
   return (
     <div className="container">
       <Header/>
-      <Search/>
+      <Search getQuery={(q)=> setQuery(q)}/>
       <CharacterGrid isLoading = {isLoading} items = {items} />
     </div>
   );
--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,10 +1,24 @@
 import React, {useState} from 'react'
 
-const Search = () => {
+
+const Search = ({getQuery}) => {
+
+    const [text , setText] = useState('') //State ändras när användaren söker genom setText(q) i onChange() funktionen nedanför
+
+    const updateQuery = (q) =>{ //Kallas när användaren söker i search-boxen genom onChange attribut i <input> elementet, q är söksträngen (query)
+        setText(q)
+        getQuery(q) //Eftersom vi vill fetcha och visa nya resultat anropar vi getQuery(q) vilket kör om fetch metoden i App.js
+    }
     return (
         <section className="search">
             <form>
-                <input type="text" className="form-control" placeholder="Search for characters" autoFocus></input>
+                <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Search for characters" 
+                value={text}
+                onChange={(e)=> updateQuery(e.target.value)}
+                />
             </form>
         </section>
     )
